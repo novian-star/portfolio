88692803dffd1c465ba693b32e9c1812db6d5f07
novian-star <humanityisyour@gmail.com>
2025-02-08 21:19:47 +0700

    ✨ feat(PapgerPage): add new component for customizable page size

---
+++ src/components/PapgerPage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    size?: 'A4';
+  }>(),
+
+  {
+    size: 'A4',
+  }
+);
+
+const pageSize = computed(() => {
+  switch (props.size) {
+    case 'A4':
+      return 'w-[210mm] h-[297mm]';
+    default:
+      return 'w-[210mm] h-[297mm]';
+  }
+});
+</script>
+
+<template>
+  <div :class="[pageSize, 'bg-white shadow']">
+    <slot />
+  </div>
+</template>
