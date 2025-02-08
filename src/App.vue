251106ae248cdd6b199435418cb1253620a974dd
novian-star <humanityisyour@gmail.com>
2025-02-08 21:20:39 +0700

    ✨ feat(App): add main application layout with SecondPage component

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SecondPage from './components/pages/SecondPage.vue';
+</script>
+
+<template>
+  <main class="w-screen h-screen bg-gray-200 overflow-x-auto">
+    <!-- Portfolio container -->
+    <section class="flex flex-col lg:items-center p-8 print:p-0">
+      <SecondPage />
+    </section>
+  </main>
+</template>
