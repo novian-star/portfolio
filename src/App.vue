c4dbc2a275f2fd9f2596fb4315b41f17e968f151
novian-star <humanityisyour@gmail.com>
2025-02-08 23:10:49 +0700

    ✨ feat(App): add print functionality and update layout for FirstPage and SecondPage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
+import { Printer } from 'lucide-vue-next';
+import FirstPage from './components/pages/FirstPage.vue';
 import SecondPage from './components/pages/SecondPage.vue';
+
+function handlePrint() {
+  window.print();
+}
 </script>
 
 <template>
-  <main class="w-screen h-screen bg-gray-200 overflow-x-auto">
+  <main class="relative w-screen h-screen">
     <!-- Portfolio container -->
-    <section class="flex flex-col lg:items-center p-8 print:p-0">
+    <section
+      class="flex flex-col md:items-center p-8 print:p-0 gap-8 overflow-auto print:gap-0"
+    >
+      <FirstPage />
+
       <SecondPage />
     </section>
+
+    <!-- Print button -->
+    <div class="fixed bottom-0 w-full p-4 print:hidden">
+      <div class="flex justify-end max-w-4xl mx-auto">
+        <button
+          class="flex items-center p-2 gap-2 bg-emerald-500 rounded shadow text-sm"
+          @click="handlePrint"
+        >
+          <Printer class="w-6 h-6 !text-white" color="white" />
+        </button>
+      </div>
+    </div>
   </main>
 </template>
